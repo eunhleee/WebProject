bbe055051d59f3c291ec6197c6cbbb672f2521a3
Seung-Gyu <61531838+Seung-Gyu@users.noreply.github.com>
2020-06-15 09:47:17 +0900

    게시판 검색 시 유지 수정

--- src/alcinfo/AcreviewMgr.java
+++ src/alcinfo/AcreviewMgr.java
@@ -13,7 +13,7 @@ public class AcreviewMgr {
 	}
 	
 	//Board Total Count:占쏙옙 占쌉시뱄옙 占쏙옙
-	public int getTotalCount(String keyField,String keyWord) {
+	public int getTotalCount(String keyField,String keyWord, int num) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -23,15 +23,17 @@ public class AcreviewMgr {
 			con = pool.getConnection();
 			if(keyWord.trim().equals("")||keyWord==null) {
 				//占싯삼옙占쏙옙 占싣닌곤옙占�
-				sql = "select count(*) from acreview ";
+				sql = "select count(*) from acreview where ac_serialnum=?";
 				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, num);
 			}
 			else {
 			//占싯삼옙占쏙옙 占쏙옙占�
 			sql = "select count(*) from acreview where "
-					+keyField+" like ?";
+					+keyField+" like ? and ac_serialnum=?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, "%"+keyWord+"%");
+			pstmt.setInt(2, num);
 			}
 			rs = pstmt.executeQuery();
 			while(rs.next()) {
